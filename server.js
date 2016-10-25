04356872445e9139e71e081659c6615df28f7fb0
Fernando Herrera <fermh2010@gmail.com>
2016-10-25 19:36:37 +0000

    completed server logic

--- server.js
+++ server.js
@@ -3,6 +3,36 @@ const app = express();
 
 app.set('port', process.env.port || 8080);
 
+app.get('/api/whoami', function(req, res) {
+    console.log(req.headers);
+    
+    var regex_result;
+    
+    var ip = req.headers['x-forwarded-for']
+        || req.connection.remoteAddress;
+    
+    const lang_raw = req.headers['accept-language'];
+    regex_result = /^([a-zA-Z-]+),/.exec(lang_raw);
+    var lang = null;
+    if(regex_result && regex_result.length > 1) {
+        lang = regex_result[1];
+    }
+    
+    const agent_raw = req.headers['user-agent'];
+    regex_result = /\(([^\)]+)\)/.exec(agent_raw);
+    var os = null;
+    if(regex_result && regex_result.length > 1) {
+        os = regex_result[1];
+    }
+    
+    var out = {
+        ipaddress: ip,
+        language: lang,
+        software: os
+    };
+    res.json(out);
+});
+
 app.listen(app.get('port'), function() {
    console.log('Server ready') ;
 });
